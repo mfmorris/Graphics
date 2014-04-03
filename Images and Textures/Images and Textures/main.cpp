ad7a1c9acf090778b94e9c7cd6adee729430c5df
Matt M <mattfmorris@gmail.com>
2014-04-02 21:30:01 -0600

    ...

--- Images and Textures/Images and Textures/main.cpp
+++ Images and Textures/Images and Textures/main.cpp
@@ -10,7 +10,10 @@ int size[2];
 
 GLubyte* bitmap;
 GLubyte* equalBitmap;
+GLubyte** equalTexture;
 GLubyte* threshBitmap;
+
+static GLuint texName;
 
 int readFileToArray() {
 	char magic1 = 'a';
@@ -59,14 +62,17 @@ int readFileToArray() {
 void equalize(int maxValue) {
 	equalized = new char*[size[1]];
 	equalBitmap = new GLubyte[size[1] * size[0]];
+	equalTexture = new GLubyte*[size[1]];
 	for (int i = 0; i < size[1]; i++) {
 		equalized[i] = new char[size[0]];
+		equalTexture[i] = new GLubyte[size[0]];
 	}
 
 	for (int i = 0; i < size[1]; i++) {
 		for (int j = 0; j < size[0]; j++) {
 			equalized[i][j] = values[i][j] * (255 / maxValue);
 			equalBitmap[(i * size[0]) + j] = (GLubyte)equalized[i][j];
+			equalTexture[i][j] = (GLubyte)equalized[i][j];
 		}
 	}
 }
@@ -91,15 +97,43 @@ void threshold() {
 }
 
 void cube() {
+	
+
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+	glBindTexture(GL_TEXTURE_2D, texName);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0, 0.0); glVertex3f(-2.0, -1.0, 0.0);
+	glTexCoord2f(0.0, 1.0); glVertex3f(-2.0, 1.0, 0.0);
+	glTexCoord2f(1.0, 1.0); glVertex3f(0.0, 1.0, 0.0);
+	glTexCoord2f(1.0, 0.0); glVertex3f(0.0, -1.0, 0.0);
+
+	glTexCoord2f(0.0, 0.0); glVertex3f(1.0, -1.0, 0.0);
+	glTexCoord2f(0.0, 1.0); glVertex3f(1.0, 1.0, 0.0);
+	glTexCoord2f(1.0, 1.0); glVertex3f(2.41421, 1.0, -1.41421);
+	glTexCoord2f(1.0, 0.0); glVertex3f(2.41421, -1.0, -1.41421);
+	glEnd();
 
 }
 
 void display(void) {
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_TEXTURE_2D);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+	glBindTexture(GL_TEXTURE_2D, texName);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0, 0.0); glVertex3f(-2.0, -1.0, 0.0);
+	glTexCoord2f(0.0, 1.0); glVertex3f(-2.0, 1.0, 0.0);
+	glTexCoord2f(1.0, 1.0); glVertex3f(0.0, 1.0, 0.0);
+	glTexCoord2f(1.0, 0.0); glVertex3f(0.0, -1.0, 0.0);
 
-	cube();
-
+	glTexCoord2f(0.0, 0.0); glVertex3f(1.0, -1.0, 0.0);
+	glTexCoord2f(0.0, 1.0); glVertex3f(1.0, 1.0, 0.0);
+	glTexCoord2f(1.0, 1.0); glVertex3f(2.41421, 1.0, -1.41421);
+	glTexCoord2f(1.0, 0.0); glVertex3f(2.41421, -1.0, -1.41421);
+	glEnd();
 	glFlush();
+	glDisable(GL_TEXTURE_2D);
+
 }
 
 void displayOriginal(void) {
@@ -129,8 +163,34 @@ void displayThreshold(void) {
 	glFlush();
 }
 
+void reshape(int w, int h) {
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60.0, (GLfloat)w / (GLfloat)h, 1.0, 30.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(0.0, 0.0, -3.6);
+}
+
+
 void init() {
 	glClearColor(0.0, 0.0, 0.0, 1.0);
+
+	//glShadeModel(GL_FLAT);
+	//glEnable(GL_DEPTH_TEST);
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	glGenTextures(1, &texName);
+	glBindTexture(GL_TEXTURE_2D, texName);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, size[1], size[0], 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, equalTexture);
 }
 
 void main(int argc, char **argv) {
@@ -147,6 +207,7 @@ void main(int argc, char **argv) {
 	glutInitWindowSize(800, 800);
 	glutCreateWindow("Textured Image");
 	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
 
 	glutInitWindowPosition(0, 0);
 	glutInitWindowSize(size[0], size[1]);
